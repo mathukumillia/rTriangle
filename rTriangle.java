bbc1cd1f66d8bc8a56f8d621d6ef2e1d2c6befd4
anirudh <mathukumillia@s.dcsdk12.org>
2013-02-11 22:27:56 -0700

    added all necessary methods

--- rTriangle.java
+++ rTriangle.java
@@ -1,7 +1,7 @@
 /**
- * HTree
+ * rTriangle
  *
- * 		recursively draws an HTree
+ * 		recursively draws an r
  *
  */
 
@@ -9,7 +9,7 @@ import java.awt.Graphics;
 
 public class rTriangle{
 	
-	private x1, y1, x2, y2, x3, y3, depth;
+	private int x1, y1, x2, y2, x3, y3, depth;
 	private Graphics g;
 
 	public static final int MAX_DEPTH = 7;
@@ -24,12 +24,118 @@ public class rTriangle{
 	public rTriangle(int x1, int y1, int x2, int y2, int x3, int y3){
 
 		this.x1 = x1;
-		this.x2 = x2;
-		this.x3 = x3;
 		this.y1 = y1;
+		this.x2 = x2;
 		this.y2 = y2;
+		this.x3 = x3;
 		this.y3 = y3;
 		this.depth = 1;
 	}
 
+	/**
+	 *   getters and setters
+	 */
+	
+	public void setX1(int x){
+		this.x1 = x;
+	}
+	public int getX1(){
+		return x1;
+	}
+	public void setX2(int x){
+		this.x2 = x;
+	}
+	public int getX2(){
+		return x2;
+	}
+	public void setX3(int x){
+		this.x3 = x;
+	}
+	public int getX3(){
+		return x3;
+	}
+	public void setY1(int y){
+		this.y1 = y;
+	}
+	public int getY1(){
+		return y1;
+	}
+	public void setY2(int y){
+		this.y2 = y;
+	}
+	public int getY2(){
+		return y2;
+	}
+	public void setY3(int y){
+		this.y3 = y;
+	}
+	public int getY3(){
+		return y3;
+	}
+	public int getDepth(){
+		return depth;
+	}
+
+	/**
+	 * incrementDepth
+	 *
+	 * 		increment the depth by one, unless the depth is greater than or equal to MAX_DEPTH, in which case it should be set to one
+	 * 
+	 */
+	public void incrementDepth(){
+		if(depth == MAX_DEPTH){
+			depth = 1;
+		}else{
+			depth++;
+		}
+	}
+
+	/**
+	 * draw
+	 * 		draw the rTriangle to the screen 
+	 * 		this is an accessor method that calls the real private recursive method
+	 *
+	 * @param g - the Graphics object associated with the window
+	 */
+	public void draw(Graphics g){
+		this.g = g;
+		recursiveDraw(depth, x1, y1, x2, y2, x3, y3);
+	}
+
+	/**
+	 *recursiveDraw
+	 *		actually draw the htree
+	 *
+	 * @param n - the current depth of the rTriangle
+	 * @param x1, y1 - coordinates of starting bottom left point
+	 * @param x2, y2 - coordinates of starting top point
+	 * @param x3, y3 - coordinates of starting bottom right point
+	 * 
+	 *
+	 * precondition - g must not be null
+	 *  
+	 */
+	private void recursiveDraw(int n, int x1, int y1, int x2, int y2, int x3, int y3){
+		//base case
+		if(n <= 0){
+			return;
+		}
+
+		int tempx;
+		int tempy;
+		//find midpoint of existing three sides
+		tempx = x1;
+		tempy = y1;
+		x1 = ((x1 + x2) / 2);
+		y1 = ((y1 + y2) / 2);
+		x2 = ((x2 + x3) / 2);
+		y2 = ((y2 + y3) / 2);
+		x3 = ((x3 + tempx) / 2);
+		y3 = ((y1 + tempy) / 2);
+		g.drawLine(x1, y1, x2, y2);
+		g.drawLine(x2, y2, x3, y3);
+		g.drawLine(x3, y3, x1, y1);
+
+		recursiveDraw(n-1, x1, y1, x2, y2, x3, y3);
+	}
 }
--- rTriangleWindow.java
+++ rTriangleWindow.java
@@ -7,10 +7,156 @@ import java.awt.event.*;
 
 class rTriangleWindow extends JFrame implements MouseListener {
 	
-	private HTree tree = null;
+	private rTriangle triangle = null;
 	
 	// double buffering objects
 	// used to eliminate flicker when resizing window
     private Image offscreen; 
 	private Graphics buffer;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param width - width of the window
+	 * @param height - height of the window
+	 * @param depth - recursive depth of the HTree
+	 */
+	public rTriangleWindow(int width, int height) {
+		super("rTriangleWindow");
+		this.setBounds(200, 200, width, height);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.addMouseListener(this);
+		this.setVisible(true);
+	}
+
+	/**
+	 * paint
+	 * 		draws the contents of the window
+	 * 
+	 * 		called automatically when the window needs updating
+	 */
+	@Override
+	public void paint(Graphics g){
+		reset();
+
+		triangle.draw(buffer);
+
+		g.drawImage(offscreen, 0, 0, this);
+	}
+
+	/**
+	 * reset
+	 *		clear the background, 
+	 *		center and scale the triangle dependent on the dimensions of the window
+	 */
+	private void reset() {
+		Rectangle b = this.getBounds();
+		
+		offscreen = createImage(b.width,b.height);
+		buffer = offscreen.getGraphics();
+		buffer.setColor(Color.white);
+		
+		buffer.clearRect(0,0,b.width,b.height); 
+		
+		if (triangle == null) { // tree not created yet 
+			triangle = new rTriangle(b.width/3, (b.height/3) * 2, (b.width/3) * 2, (b.height/3), b.width, (b.height/3) * 2);
+		} else {
+			triangle.setX1(b.width / 2);
+			triangle.setY1(b.height / 2 + 12);
+		}
+	}
+
+	/**
+	 * update
+	 * 		paint the window on each update,
+	 * 		necessary for double buffering
+	 */
+	@Override
+	public void update(Graphics g) {
+		paint(g);
+	}
+
+	/**
+	 * main
+	 * 		create an rTriangle window with an appropriate recursive depth
+	 * 
+	 * @param args - args[1] is the depth of recursion
+	 */
+	public static void main(String[] args) {
+		rTriangleWindow rtwin = new rTriangleWindow(600, 520);
+
+	}
+
+	/**
+	 * incrementDepth
+	 * 		increase the depth of the rTriangle, up to a point
+	 */
+	public void incrementDepth() {
+		triangle.incrementDepth();
+		repaint();
+	}
+
+
+	/**
+	 * mouseClicked
+	 * 		mouse was clicked (pressed and released) on the window
+	 * 		
+	 * 		must be overwritten when implementing MouseListener
+	 * 
+	 * @param e - object holding info about the mouse event
+	 */
+	public void mouseClicked(MouseEvent e) {
+		incrementDepth();
+	}
+
+
+	/**
+	 * mousePressed
+	 * 		mouse was pressed down
+	 * 		
+	 * 		must be overwritten when implementing MouseListener
+	 *
+	 * @param e - object holding info about the mouse event
+	 */
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	/**
+	 * mouseReleased
+	 * 		mouse was released after a press
+	 * 
+	 * 		must be overwritten when implementing MouseListener
+	 *
+	 * @param e - object holding info about the mouse event
+	 */
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	/**
+	 * mouseEntered
+	 * 		mouse entered the window
+	 * 
+	 * 		must be overwritten when implementing MouseListener
+	 *
+	 * @param e - object holding info about the mouse event
+	 */
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	/**
+	 * mouseExited
+	 * 		mouse exited the window
+	 * 
+	 * 		must be overwritten when implementing MouseListener
+	 *
+	 * @param e - object holding info about the mouse event
+	 */
+	public void mouseExited(MouseEvent e) {
+
+	}
+
 }
